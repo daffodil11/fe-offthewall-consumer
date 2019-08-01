7f2546e697659b7762c41b232cd2a6a4f7809e25
ian <kennaleeds@gmail.com>
2019-08-01 17:07:17 +0100

    adds floatingactionbutton on main activity

--- app/src/main/java/com/slick/offthewall/MainActivity.java
+++ app/src/main/java/com/slick/offthewall/MainActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView hello = (TextView) findViewById(R.id.hello);
-        Intent mapIntent = new Intent(this, MapsActivity.class);
-        hello.setOnClickListener(view -> {
-            startActivity(mapIntent);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapActivity();
+            }
         });
+    }
+    public void openMapActivity() {
+        Intent mapIntent = new Intent(this, MapsActivity.class);
+        startActivity(mapIntent);
+
     }
 }
