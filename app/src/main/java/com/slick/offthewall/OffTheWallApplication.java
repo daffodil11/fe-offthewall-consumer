17efd6b6e114f64f3790fe136abb045878801341
Chloe <c.williams22@hotmail.co.uk>
2019-08-02 12:11:40 +0100

    implements apollo to create a wall graphql query

---
+++ app/src/main/java/com/slick/offthewall/OffTheWallApplication.java
@@ -0,0 +1,27 @@
+package com.slick.offthewall;
+
+
+import android.app.Application;
+
+import com.apollographql.apollo.ApolloClient;
+import okhttp3.OkHttpClient;
+
+public class OffTheWallApplication extends Application {
+    private static final String BASE_URL = "https://offthewall-teamslick.herokuapp.com/graphql";
+    private ApolloClient apolloClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+
+        apolloClient = ApolloClient.builder()
+                .serverUrl(BASE_URL)
+                .okHttpClient(okHttpClient)
+                .build();
+    }
+
+    public ApolloClient getApolloClient() {
+        return apolloClient;
+    }
+}
