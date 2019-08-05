60af1f4bc86fe5a387e31d3484747c8855a2a49e
Simon Davey <simonr.davey@gmail.com>
2019-08-05 17:43:42 +0100

    Begins to set up AugmentedArtFragment.

--- app/src/main/java/com/slick/offthewall/AugmentedArtFragment.java
+++ app/src/main/java/com/slick/offthewall/AugmentedArtFragment.java
@@ -1,6 +1,33 @@
 package com.slick.offthewall;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.google.ar.sceneform.ux.ArFragment;
 
 public class AugmentedArtFragment extends ArFragment {
+
+    private static final String TAG = "AugmentedArtFragment";
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(context, "Sceneform requires Android N or later", Toast.LENGTH_LONG);
+        }
+
+        String openGlVersionString =
+                ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 or later");
+            Toast.makeText(context, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG);
+        }
+    }
 }
