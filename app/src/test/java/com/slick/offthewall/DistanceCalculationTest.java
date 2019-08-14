51c74f077ec7b188ea46dc812923e452453553bd
Simon Davey <simonr.davey@gmail.com>
2019-08-14 17:03:06 +0100

    Adds unit test for the getDistanceFrom method of Wall. Passing.

---
+++ app/src/test/java/com/slick/offthewall/DistanceCalculationTest.java
@@ -0,0 +1,19 @@
+package com.slick.offthewall;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DistanceCalculationTest {
+    @Test
+    public void distance_sortsCorrectly() {
+        Wall wallInLeeds = new Wall(1, 53.7949778, -15449472);
+        int distanceToYork = wallInLeeds.getDistanceFrom(53.959608, -1.084120);
+        int distanceToSheffield = wallInLeeds.getDistanceFrom(53.371441, -1.523469);
+        int distanceToWakefield = wallInLeeds.getDistanceFrom(53.682158, -1.497149);
+        int distanceToPudsey = wallInLeeds.getDistanceFrom(53.799889, -1.673273);
+        assertTrue(distanceToYork < distanceToSheffield);
+        assertTrue(distanceToWakefield < distanceToSheffield);
+        assertTrue(distanceToPudsey < distanceToWakefield);
+    }
+}
