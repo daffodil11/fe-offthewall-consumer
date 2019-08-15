e73bfe1f92e9b1863d81d89d6cd8dd4dcf58fca5
Simon Davey <simonr.davey@gmail.com>
2019-08-15 09:56:07 +0100

    Adds a method for getting artist by id from the approved artist list.

--- app/src/main/java/com/slick/offthewall/OffTheWallApplication.java
+++ app/src/main/java/com/slick/offthewall/OffTheWallApplication.java
@@ -4,15 +4,22 @@ package com.slick.offthewall;
 import android.app.Application;
 
 import com.apollographql.apollo.ApolloClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import okhttp3.OkHttpClient;
 
 public class OffTheWallApplication extends Application {
     private static final String BASE_URL = "https://offthewall-teamslick.herokuapp.com/graphql";
     private static ApolloClient apolloClient;
+    private static final Map<Integer, String> APPROVED_ARTISTS = new HashMap<Integer,String>();
 
     @Override
     public void onCreate() {
         super.onCreate();
+        APPROVED_ARTISTS.put(1, "bobbirae");
+        APPROVED_ARTISTS.put(2, "mikitillett");
         OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
 
         apolloClient = ApolloClient.builder()
@@ -23,5 +30,9 @@ public class OffTheWallApplication extends Application {
 
     public static ApolloClient getApolloClient() {
         return apolloClient;
+    }
+
+    public static String getApprovedArtistById(int id) {
+        return APPROVED_ARTISTS.get(id);
     }
 }
